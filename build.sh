0ca35e1fe11bc2eba114b7f9dbb230024b6f3891
James <james@resin.io>
2016-01-29 11:43:19 +0700

    Fix broken build: - No longer bind mount docker.sock socket in build container. - Separating 2 debootstrap stages to prevent hanging during the build.

--- build.sh
+++ build.sh
@@ -2,37 +2,10 @@
 
 set -o errexit
 
-SUITES='wheezy jessie'
-MIRROR='ftp://ftp.debian.org/debian/'
-REPO='resin/armel-debian'
-LATEST='jessie'
+dir=$(mktemp --tmpdir=/var/tmp -d)
 
-curl -SLO https://github.com/resin-io/qemu/releases/download/$QEMU_VERSION/qemu-$QEMU_VERSION.tar.gz \
-	&& echo "$QEMU_SHA256  qemu-$QEMU_VERSION.tar.gz" > qemu-$QEMU_VERSION.tar.gz.sha256sum \
-	&& sha256sum -c qemu-$QEMU_VERSION.tar.gz.sha256sum \
-	&& tar -xz --strip-components=1 -f qemu-$QEMU_VERSION.tar.gz
-
-for suite in $SUITES; do
-	dir=$(mktemp --tmpdir=/var/tmp -d)
-	date=$(date +'%Y%m%d' -u)
+mkdir -p $dir/rootfs/usr/bin
+cp qemu-arm-static $dir/rootfs/usr/bin
+chmod +x $dir/rootfs/usr/bin/qemu-arm-static
 	
-	mkdir -p $dir/rootfs/usr/bin
-	cp qemu-arm-static $dir/rootfs/usr/bin
-	chmod +x $dir/rootfs/usr/bin/qemu-arm-static
-	
-	./mkimage.sh -t $REPO:$suite --dir=$dir debootstrap --variant=minbase --arch=armel --include=sudo $suite $MIRROR
-	rm -rf $dir
-
-	docker run --rm $REPO:$suite bash -c 'dpkg-query -l' > $suite
-
-	# Upload to S3 (using AWS CLI)
-	printf "$ACCESS_KEY\n$SECRET_KEY\n$REGION_NAME\n\n" | aws configure
-	aws s3 cp $suite s3://$BUCKET_NAME/image_info/armel-debian/$suite/
-	aws s3 cp $suite s3://$BUCKET_NAME/image_info/armel-debian/$suite/$suite_$date
-	rm -f $suite
-	
-	docker tag -f $REPO:$suite $REPO:$suite-$date
-	if [ $LATEST == $suite ]; then
-		docker tag -f $REPO:$suite $REPO:latest
-	fi
-done
+./mkimage.sh -t $REPO:$suite --dir=$dir debootstrap --foreign --variant=minbase --arch=armel --include=sudo $suite $MIRROR
